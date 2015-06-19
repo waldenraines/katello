df7dfc9df36878da6eef42b9b0f68bcfd67bf3ae
Walden Raines <walden@redhat.com>
2015-06-20 01:06:32 +0530

    Fixes BZ1219263: patch active record to fix datetime timezone issue.
    https://bugzilla.redhat.com/show_bug.cgi?id=1219263

--- lib/monkeys/rails_3.2.8_patches.rb
+++ lib/monkeys/rails_3.2.8_patches.rb
@@ -1,7 +1,7 @@
+
+# Patch from https://github.com/rails/rails/issues/6865
 module ActiveRecord
   module Calculations
-
-    # Patch from https://github.com/rails/rails/issues/6865
     def perform_calculation(operation, column_name, options = {})
       operation = operation.to_s.downcase
 
@@ -24,6 +24,32 @@ module ActiveRecord
         execute_grouped_calculation(operation, column_name, distinct)
       else
         execute_simple_calculation(operation, column_name, distinct)
+      end
+    end
+  end
+end
+
+# Patch from https://github.com/rails/rails/issues/9678
+module ActiveSupport
+  class TimeZone
+    def parse(str, now=now)
+      parts = Date._parse(str, false)
+      return if parts.empty?
+
+      time = Time.utc(
+          parts.fetch(:year, now.year),
+          parts.fetch(:mon, now.month),
+          parts.fetch(:mday, now.day),
+          parts.fetch(:hour, 0),
+          parts.fetch(:min, 0),
+          parts.fetch(:sec, 0),
+          parts.fetch(:sec_fraction, 0) * 1000000
+      )
+
+      if parts[:offset]
+        TimeWithZone.new(time - parts[:offset], self)
+      else
+        TimeWithZone.new(nil, self, time)
       end
     end
   end
