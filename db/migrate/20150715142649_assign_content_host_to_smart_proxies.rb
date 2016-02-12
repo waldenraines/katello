e081e538cb7726c6379b3e61859c1dd7803c8932
Walden Raines <walden@redhat.com>
2016-02-12 13:07:23 -0500

    hasdfh

--- db/migrate/20150715142649_assign_content_host_to_smart_proxies.rb
+++ db/migrate/20150715142649_assign_content_host_to_smart_proxies.rb
@@ -2,12 +2,12 @@ class AssignContentHostToSmartProxies < ActiveRecord::Migration
   class System < ActiveRecord::Base
     self.table_name = "katello_systems"
   end
-  
+
   def up
     SmartProxy.reset_column_information
 
     SmartProxy.all.each do |proxy|
-      content_host = System.where(:name => proxy.name).order("created_at DESC").first
+      content_host = AssignContentHostToSmartProxies::System.where(:name => proxy.name).order("created_at DESC").first
 
       if content_host
         proxy.content_host_id = content_host.id
